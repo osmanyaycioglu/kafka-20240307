b6c6ba8c2c8024ebcb5acbfe31c8909241e90fab
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-03-08 11:42:44 +0300

    day 2 1

---
+++ kafka-spring/src/main/java/org/training/kafka/kafka/spring/KafkaSpringApplication.java
@@ -0,0 +1,14 @@
+package org.training.kafka.kafka.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringApplication.class,
+                              args);
+    }
+
+}
---
+++ kafka-spring/src/test/java/org/training/kafka/kafka/spring/KafkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.kafka.kafka.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
